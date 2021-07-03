3c5fdaa290d57d6aae6ca680d5107f236960f913
Victoria Altomare vsaltomare@gmail.com <vsaltomare@gmail.com>
2021-07-03 12:10:06 +1000

    Seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'Faker'
+User.destroy_all
+
+5.times do
+  User.create(
+    email:Faker::Internet.email,
+    password: "password",
+    first_name:Faker::FunnyName.name,
+    last_name:Faker::FunnyName.name,
+    about_me:Faker::Quote,
+    discipline: ["crossfit", "boxing", "weights"].sample
+    level: ["beginner", "intermediate", "advanced"].sample
+    price: rand(20..100)
+    )
+end
