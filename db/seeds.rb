6ad40584426d09e3da85590213ba3992606ad85f
Tom Nijam <60531002+ewigkeiten32@users.noreply.github.com>
2021-07-03 14:40:56 +1000

    Revert "Seeding users"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'Faker'
-User.destroy_all
-
-5.times do
-  user = User.create!(
-    email:Faker::Internet.email,
-    password: "password",
-    first_name:Faker::FunnyName.name,
-    last_name:Faker::FunnyName.name)
-    about_me:Faker::Quote.famous_last_words
-
-  trainer = Trainer.create!(
-    user: user,
-    about_me:Faker::Quote.famous_last_words,
-    discipline: ["crossfit", "boxing", "weights"].sample,
-    level: ["beginner", "intermediate", "advanced"].sample,
-    price: rand(20..100)
-    )
-end
-  booking = Booking.create!(
-    user_id: User.first.id,
-    trainer_id: Trainer.last.id,
-    start_time: Date.today,
-    end_time: Date.today + 3.hours,
-    )
