e5e7809d8d4bff56ef47f37dc159814864467d0a
Tom Nijam <thomas.nijam@gmail.com>
2021-07-10 13:25:53 +1000

    bug fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,7 @@ num = 1
 
 20.times do
 
-  file= URI.open('https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
--   # user= User.new()
-  # file= URI.open('https://thispersondoesnotexist.com/image')
+  file = URI.open('https://thispersondoesnotexist.com/image')
   user = User.create!(
     email:Faker::Internet.email,
     password: "password",
